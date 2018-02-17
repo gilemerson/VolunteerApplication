02f692a6ff04b65b5d53266ab71b55a1537ea982
Gil Emerson <gemerson@gilemerson.ca>
2018-02-17 14:50:08 -0500

    Volunteer App - Show Default image Functionality Added

--- src/views/NewUserViewController.java
+++ src/views/NewUserViewController.java
@@ -1,17 +1,23 @@
 
 package views;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ResourceBundle;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javax.imageio.ImageIO;
 import models.Volunteer;
 
 /**
@@ -26,6 +32,9 @@ public class NewUserViewController implements Initializable {
     @FXML private TextField phoneTextField;
     @FXML private DatePicker birthday;
     @FXML private Label errMsgLabel;
+    @FXML private ImageView imageView;
+    
+    private File imageFile;
     
     
     /**
@@ -53,6 +62,19 @@ public class NewUserViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         errMsgLabel.setText("");
+        
+        //load the default image for the avatar
+        try 
+        {
+            imageFile = new File("./src/images/defaultPerson.png");
+            BufferedImage bufferedImage = ImageIO.read(imageFile);
+            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+            imageView.setImage(image);
+        }
+        catch(IOException e)
+        {
+            System.err.println(e.getMessage());
+        }
     }    
     
 }
